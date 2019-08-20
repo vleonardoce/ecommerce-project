864afa12988f7098c15444fd55980b2544e2f849
Victor Leonardo Cordero Espinoza <vcordero@belatrix.com>
2019-08-20 16:13:36 -0500

    ecommerce-app

---
+++ projects/ecommerce-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ecommerce-app';
+}
