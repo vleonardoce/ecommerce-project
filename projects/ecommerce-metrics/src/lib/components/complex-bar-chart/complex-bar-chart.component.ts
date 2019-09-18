2c47d718444dce25739fecd6274c106fbba0a18f
jordinola <jordinola@JORDINOLA>
2019-09-18 13:25:52 -0500

    charts separated in library

---
+++ projects/ecommerce-metrics/src/lib/components/complex-bar-chart/complex-bar-chart.component.ts
@@ -0,0 +1,113 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-complex-bar-chart',
+  templateUrl: './complex-bar-chart.component.html',
+  styleUrls: ['./complex-bar-chart.component.css']
+})
+export class ComplexBarChartComponent implements OnInit {
+
+  view: any[] = [600, 400];
+  // options for the chart
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Product';
+  showYAxisLabel = true;
+  yAxisLabel = 'Sales';
+  timeline = true;
+  colorScheme = {
+    domain: ['#9370DB', '#87CEFA', '#FA8072', '#FF7F50', '#90EE90', '#9370DB']
+  };
+  //pie
+  showLabels = true;
+  // data goes here
+  public multi = [
+    {
+      "name": "IPhone X",
+      "series": [
+        {
+          "name": "2019",
+          "value": 120
+        },
+        {
+          "name": "2018",
+          "value": 100
+        }
+      ]
+    },
+    {
+      "name": "Samsung S9",
+      "series": [
+        {
+          "name": "2019",
+          "value": 200
+        },
+        {
+          "name": "2018",
+          "value": 90
+        }
+      ]
+    },
+    {
+      "name": "Samsung S9 Plus",
+      "series": [
+        {
+          "name": "2019",
+          "value": 150
+        },
+        {
+          "name": "2018",
+          "value": 110
+        }
+      ]
+    },
+    {
+      "name": "Samsung S10",
+      "series": [
+        {
+          "name": "2019",
+          "value": 100
+        },
+        {
+          "name": "2018",
+          "value": 90
+        }
+      ]
+    },
+    {
+      "name": "Samsung S10 Plus",
+      "series": [
+        {
+          "name": "2019",
+          "value": 210
+        },
+        {
+          "name": "2018",
+          "value": 100
+        }
+      ]
+    },
+    {
+      "name": "Samsung Note 10 Plus",
+      "series": [
+        {
+          "name": "2019",
+          "value": 100
+        },
+        {
+          "name": "2018",
+          "value": 0
+        }
+      ]
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/ecommerce-metrics/src/lib/ecommerce-metrics.module.ts
+++ projects/ecommerce-metrics/src/lib/ecommerce-metrics.module.ts
@@ -4,13 +4,15 @@ import { SimpleBarChartComponent } from './components/simple-bar-chart/simple-ba
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ComplexFullBarChartComponent } from './components/complex-full-bar-chart/complex-full-bar-chart.component';
+import { ComplexBarChartComponent } from './components/complex-bar-chart/complex-bar-chart.component';
 
 @NgModule({
-  declarations: [EcommerceMetricsComponent, SimpleBarChartComponent],
+  declarations: [EcommerceMetricsComponent, SimpleBarChartComponent, ComplexFullBarChartComponent, ComplexBarChartComponent],
   imports: [
     NgxChartsModule,
     BrowserAnimationsModule
   ],
-  exports: [EcommerceMetricsComponent, SimpleBarChartComponent]
+  exports: [EcommerceMetricsComponent, SimpleBarChartComponent, ComplexFullBarChartComponent, ComplexBarChartComponent]
 })
 export class EcommerceMetricsModule { }
